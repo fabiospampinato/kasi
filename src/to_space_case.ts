ec88f67e9e5a72b56b4ea8391a9825e1db31e279
Hyper-Z11 <114817308+hyperz111@users.noreply.github.com>
2026-01-22 05:48:16 +0700

    toTitleCase: aligned with lodash, preserving fully-uppercase words

--- src/to_space_case.ts
+++ src/to_space_case.ts
@@ -6,9 +6,11 @@ const splitRe = /(^|[a-z ])([A-Z]+)(?=([a-z])?)/g;
 
 /* MAIN */
 
-const toSpaceCase = ( value: string ): string => {
+const toSpaceCase = ( value: string, preserveCase = false ): string => {
 
-  return value.replace ( separatorRe, ' ' ).replace ( splitRe, ( _, $1, $2, $3 ) => $3 && $2.length > 1 ? `${$1.trim ()} ${$2.slice ( 0, -1 )} ${$2.slice ( -1 )}` : `${$1.trim ()} ${$2}` ).toLowerCase ().trim ();
+  const transform = ( text: string ): string => preserveCase ? text : text.toLowerCase ();
+
+  return transform ( value.replace ( separatorRe, ' ' ).replace ( splitRe, ( _, $1, $2, $3 ) => $3 && $2.length > 1 ? `${$1.trim ()} ${$2.slice ( 0, -1 )} ${$2.slice ( -1 )}` : `${$1.trim ()} ${$2}` ) ).trim ();
 
 };
 
